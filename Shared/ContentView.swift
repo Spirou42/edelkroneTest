b73c5846fe36f90a3946774a4c6f9b76ea9dec39
Carsten Müller <carstenm@mac.com>
2022-03-02 20:47:21 +0100

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Carsten Müller on 02.03.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/edelkroneTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  edelkroneTestApp.swift
+//  Shared
+//
+//  Created by Carsten Müller on 02.03.22.
+//
+
+import SwiftUI
+
+@main
+struct edelkroneTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
