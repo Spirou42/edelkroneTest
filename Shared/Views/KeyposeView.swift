9d36de416cb624a4eb20de329499e716036697fb
Carsten Müller <carstenm@mac.com>
2024-02-07 20:55:24 +0100

    Better Button

--- Shared/Views/KeyposeView.swift
+++ Shared/Views/KeyposeView.swift
@@ -14,81 +14,69 @@ struct KeyposeView: View {
   @ObservedObject var slot:KeyposeSlot
   var body: some View {
     ZStack(){
-      
+
       Button( action: {print("Action for %d",slot.index)}){
         Text("")
           .font(.applicationFont(.title))
           .frame(maxWidth:.infinity, maxHeight: .infinity, alignment: .center)
+        
+        VStack(alignment: .leading, spacing: 1){
+          
+          HStack(alignment: .center, spacing: 6.0 ){
+            Text("Pose ").frame(width:50,alignment: .leading)
+              .shadow(color:.white, radius: 0.15, x:0.7, y:0.7)
+              .shadow(color:.lightGray, radius: 0.15, x:-0.7, y:-0.7)
+              .font(.applicationFont(.poseTitle))
+            Text(String(slot.index))
+              .shadow(color:.white, radius: 0.15, x:0.7, y:0.7)
+              .shadow(color:.lightGray, radius: 0.15, x:-0.7, y:-0.7)
+              .font(.applicationFont(.poseTitle))
+          }.frame(maxWidth: .infinity, alignment: .topLeading)
+            .padding([.top],5).padding([.leading],10)
+          Spacer().frame(height: slot.isFilled ? 4 : 65)
+          if(slot.isFilled){
+            ForEach(Array(slot.axels.keys).sorted(by: {return $0 < $1})){ t in
+              HStack(spacing: 0){
+//                            Text("Axel:").frame(width:50,alignment: .trailing)
+//                              .shadow(color:.white, radius: 0.15, x:0.7, y:0.7)
+//                              .shadow(color:.lightGray, radius: 0.15, x:-0.7, y:-0.7)
+//                            .font(.applicationFont(.poseTitle2))
+                Text(slot.axels[t]!?.axelName.toString() ?? "").frame(width:40,alignment:.leading)
+                  .shadow(color:.white, radius: 0.15, x:0.7, y:0.7)
+                  .shadow(color:.lightGray, radius: 0.15, x:-0.7, y:-0.7)
+                  .padding([.leading], 0)
+                  .font(.applicationFont(.poseTitle2))
+                Text(" : ").fixedSize(horizontal: true, vertical: false)
+                  .padding([.leading] ,2)
+                  .padding([.trailing],2)
+                  .shadow(color:.white, radius: 0.15, x:0.7, y:0.7)
+                  .shadow(color:.lightGray, radius: 0.15, x:-0.7, y:-0.7)
+                  .font(.applicationFont(.poseTitle2))
+                
+                Text(String(format:"%03.02f",slot.axels[t]!?.axelPosition ?? 0.0)).monospacedDigit().frame(width:60,alignment: .leading)
+                  .shadow(color:.white, radius: 0.15, x:0.7, y:0.7)
+                  .shadow(color:.lightGray, radius: 0.15, x:-0.7, y:-0.7)
+                  .font(.applicationFont(.poseTitle2))
+                
+                Text((slot.axels[t]!?.calibrated ?? false) ? "X" : "O")
+                  .shadow(color:.white, radius: 0.15, x:0.7, y:0.7)
+                  .shadow(color:.lightGray, radius: 0.15, x:-0.7, y:-0.7)
+                  .font(.applicationFont(.poseTitle2))
+              }.frame(maxWidth: .infinity, alignment: .topLeading)
+                .padding([.leading],18)
+            }
+            Spacer()
+          }
+        }
+
       }
       .buttonStyle(ColoredButtonStyle(buttonColor:  slot.isFilled ? Color.green.withAlpha(0.1) : Color.blue.withAlpha(0.1),
-                                      labelColor: Color.orange,
-                                      cornerRadius: 7,
+                                      labelColor: Color.black,
+                                      cornerRadius: 3,
                                       shadowRadius: 0
                                      ))
 
-      VStack(alignment: .leading, spacing: 1){
-        
-        HStack(alignment: .center, spacing: 6.0 ){
-          Text("Pose ").frame(width:50,alignment: .leading)
-            .shadow(color:.white, radius: 0.15, x:0.7, y:0.7)
-            .shadow(color:.lightGray, radius: 0.15, x:-0.7, y:-0.7)
-            .font(.applicationFont(.poseTitle))
-          Text(String(slot.index))
-            .shadow(color:.white, radius: 0.15, x:0.7, y:0.7)
-            .shadow(color:.lightGray, radius: 0.15, x:-0.7, y:-0.7)
-            .font(.applicationFont(.poseTitle))
-        }.frame(maxWidth: 127, alignment: .topLeading)
-          .padding([.top],5)
-        Spacer().frame(height: slot.isFilled ? 1 : 65)
-        if(slot.isFilled){
-          ForEach(Array(slot.axels.keys).sorted(by: {return $0 < $1})){ t in
-            HStack(spacing: 0){
-              //            Text("Axel:").frame(width:50,alignment: .trailing)
-              //              .shadow(color:.white, radius: 0.15, x:0.7, y:0.7)
-              //              .shadow(color:.lightGray, radius: 0.15, x:-0.7, y:-0.7)
-              //            .font(.applicationFont(.poseTitle2))
-              Text(slot.axels[t]!?.axelName.toString() ?? "").frame(width:40,alignment:.leading)
-                .shadow(color:.white, radius: 0.15, x:0.7, y:0.7)
-                .shadow(color:.lightGray, radius: 0.15, x:-0.7, y:-0.7)
-                .padding([.leading], 0)
-                .font(.applicationFont(.poseTitle2))
-              Text(" : ").fixedSize(horizontal: true, vertical: false)
-                .padding([.leading] ,2)
-                .padding([.trailing],2)
-                .shadow(color:.white, radius: 0.15, x:0.7, y:0.7)
-                .shadow(color:.lightGray, radius: 0.15, x:-0.7, y:-0.7)
-                .font(.applicationFont(.poseTitle2))
-              
-              Text(String(format:"%03.02f",slot.axels[t]!?.axelPosition ?? 0.0)).monospacedDigit().frame(width:60,alignment: .leading)
-                .shadow(color:.white, radius: 0.15, x:0.7, y:0.7)
-                .shadow(color:.lightGray, radius: 0.15, x:-0.7, y:-0.7)
-                .font(.applicationFont(.poseTitle2))
-              
-              Text((slot.axels[t]!?.calibrated ?? false) ? "X" : "O")
-                .shadow(color:.white, radius: 0.15, x:0.7, y:0.7)
-                .shadow(color:.lightGray, radius: 0.15, x:-0.7, y:-0.7)
-                .font(.applicationFont(.poseTitle2))
-            }
-          }
-          Spacer()
-        }
-      }
-      //.frame(minHeight: 44)
-      
-      //Rectangle().fill(Color.lightLightGray)
-  
-//      Button( action: {print("Action for %d",slot.index)}){
-//        Text("")
-//          .font(.applicationFont(.title))
-//          .frame(maxWidth:.infinity, maxHeight: .infinity, alignment: .center)
-//      }
-//      .buttonStyle(ColoredButtonStyle(buttonColor:  Color.clear,
-//                                      labelColor: Color.orange,
-//                                      cornerRadius: 7,
-//                                      shadowRadius: 0
-//                                     ))
-      //.frame(width: 130, height: 41)
-      
+
       
     }.frame(width: 180, height: 90, alignment: .center)
       .background(
@@ -124,7 +112,7 @@ struct KeyposeList: View {
         
       }
     }
-    .frame(width: 638,height:170)
+    .frame(width: 638,height:210)
     .fixedSize(horizontal: false, vertical: true)
     
   }
@@ -132,7 +120,7 @@ struct KeyposeList: View {
 
 struct KeyposeView_Previews: PreviewProvider {
   static var previews: some View {
-    KeyposeView(slot: KeyposeSlot(0  , status: MotionControlStatus() ) )
+    KeyposeView(slot: KeyposeSlot(1  , status: MotionControlStatus() ) )
   }
 }
 
