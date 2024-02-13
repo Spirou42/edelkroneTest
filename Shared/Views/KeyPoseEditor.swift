42917954b3433cf37260af1a3ff7765f8ad420e8
Carsten Müller <carstenm@mac.com>
2024-02-13 21:14:05 +0100

    Keypose editor

---
+++ Shared/Views/KeyPoseEditor.swift
@@ -0,0 +1,18 @@
+//
+//  KeyPoseEditor.swift
+//  edelkroneTest (macOS)
+//
+//  Created by Carsten Müller on 12.02.24.
+//
+
+import SwiftUI
+
+struct KeyPoseEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    KeyPoseEditor()
+}
